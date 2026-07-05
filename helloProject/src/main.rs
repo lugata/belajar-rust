1a04cfc115a364b42fa1dcbbfc63b3c63703c212
Dauw Bastha Fiastat Lugata <ata040602@gmail.com>
2026-07-05 18:46:11 +0700

    shadowing

--- helloProject/src/main.rs
+++ helloProject/src/main.rs
@@ -1,15 +1,10 @@
 fn main() {
-    println!("Hello, world!");
     let x = 5;
+    let x = x + 1;
+    let x = x * 200;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
     println!("The value of x is: {}", x);
-    const Y: u16 = 10;
-    println!("The value of Y is: {}", Y);
-    println!("The value of PI is: {}", PI);
-    println!(
-        "The value of THREE_HOURS_IN_SECONDS is: {}",
-        THREE_HOURS_IN_SECONDS
-    );
 }
-
-const PI: f64 = 3.141592653589793;
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
