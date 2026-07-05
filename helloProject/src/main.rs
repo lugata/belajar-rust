79685e0c41636f745397a4636b19a60a1c766adc
Dauw Bastha Fiastat Lugata <ata040602@gmail.com>
2026-07-05 19:04:08 +0700

    if else expression

--- helloProject/src/main.rs
+++ helloProject/src/main.rs
@@ -1,10 +1,27 @@
+#![allow(warnings)]
 fn main() {
-    let x = 5;
-    let x = x + 1;
-    let x = x * 200;
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
-    println!("The value of x is: {}", x);
+    // let age: u8 = 16;
+    // println!("The value of age is: {}", age);
+    // if age >= 18 {
+    //     println!("You can drive a car.");
+    // } else if age >= 16 {
+    //     println!("You can drive a car with a learner's permit.");
+    // } else {
+    //     println!("You can't drive a car.");
+    // }
+
+    // let number: i32 = 991;
+    // if number % 4 == 0 {
+    //     println!("The number is divisible by 4.");
+    // } else if number % 3 == 0 {
+    //     println!("The number is divisible by 3.");
+    // } else if number % 2 == 0 {
+    //     println!("The number is divisible by 2.");
+    // } else {
+    //     println!("The number is not divisible by 4, 3, or 2.");
+    // }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {number}");
 }
