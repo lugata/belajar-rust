77a2a70c8c2e631555de13b1702fd31e2e40ca51
Dauw Bastha Fiastat Lugata <ata040602@gmail.com>
2026-07-05 17:02:28 +0700

    variables dan mutability

--- helloProject/src/main.rs
+++ helloProject/src/main.rs
@@ -1,33 +1,7 @@
 fn main() {
-    let mut account = BankAccount {
-        account_number: "123456789".to_string(),
-        balance: 1000.0,
-    };
-    account.check_balance();
-
-    account.withdraw(50.0);
-
-    account.check_balance();
-}
-
-struct BankAccount {
-    account_number: String,
-    balance: f64,
-}
-
-impl BankAccount {
-    fn withdraw(&mut self, amount: f64) {
-        println!(
-            "Withdrawing Rp. {} from account {}",
-            amount, self.account_number
-        );
-        self.balance -= amount;
-    }
-
-    fn check_balance(&self) {
-        println!(
-            "Account {} has a balance of Rp. {}",
-            self.account_number, self.balance
-        );
-    }
+    println!("Hello, world!");
+    let mut a: u16 = 5;
+    println!("a is: {}", a);
+    a = 10;
+    println!("a is: {}", a);
 }
