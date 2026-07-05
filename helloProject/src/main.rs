843d89e28a5d7ceb7ffbf5e01bcaef71e7887097
Dauw Bastha Fiastat Lugata <ata040602@gmail.com>
2026-07-05 17:10:23 +0700

    constants

--- helloProject/src/main.rs
+++ helloProject/src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
     println!("Hello, world!");
-    let mut a: u16 = 5;
-    println!("a is: {}", a);
-    a = 10;
-    println!("a is: {}", a);
+    let x = 5;
+    println!("The value of x is: {}", x);
+    const Y: u16 = 10;
+    println!("The value of Y is: {}", Y);
+    println!("The value of PI is: {}", PI);
+    println!(
+        "The value of THREE_HOURS_IN_SECONDS is: {}",
+        THREE_HOURS_IN_SECONDS
+    );
 }
+
+const PI: f64 = 3.141592653589793;
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
