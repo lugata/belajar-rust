c489e8155ae16215c3957059ad18cdea5a9482fb
Dauw Bastha Fiastat Lugata <ata040602@gmail.com>
2026-07-05 15:17:02 +0700

    ownership

--- helloProject/src/main.rs
+++ helloProject/src/main.rs
@@ -1,38 +1,13 @@
 fn main() {
-    hello_world();
-    tell_height(180);
-    human_id("Alice", 30, 165.2);
-    let x = {
-        let price = 100;
-        let qty = 5;
-        price * qty
-    };
-    println!("The total is: {}", x);
-    let sum = add(5, 10);
-    println!("The sum is: {}", sum);
-
-    let weight = 70.0;
-    let height = 1.75;
-    let bmi = BMI(weight, height);
-    println!("The BMI is: {:.2}", bmi);
+    let s1 = String::from("RUST");
+    let len = calculate_length(&s1);
+    println!("The length is: {}, s1 is: {}", s1, len);
 }
 
-fn hello_world() {
-    println!("Hello, World!");
+fn print_lost(s: &String) {
+    println!("The lost string is: {}", &s1);
 }
 
-fn tell_height(height: i32) {
-    println!("The height is: {} cm", height);
-}
-
-fn human_id(name: &str, age: u32, height: f32) {
-    println!("Name: {}, Age: {}, Height: {} cm", name, age, height);
-}
-
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-fn BMI(weight: f64, height: f64) -> f64 {
-    weight / (height * height)
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
