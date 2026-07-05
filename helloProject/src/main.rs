3f6a40af97b4dbd3bf06bfe258732ca88cbb81fe
Dauw Bastha Fiastat Lugata <ata040602@gmail.com>
2026-07-05 16:58:12 +0700

    borrowing with struct

--- helloProject/src/main.rs
+++ helloProject/src/main.rs
@@ -1,10 +1,33 @@
 fn main() {
-    let mut _x = 5;
-    println!("x is: {}", _x);
-    let _r = &mut _x;
-    println!("r is: {}", _r);
-    *_r += 1;
-    println!("r is: {}", _r);
-    *_r += 3;
-    println!("r is: {}", _r);
+    let mut account = BankAccount {
+        account_number: "123456789".to_string(),
+        balance: 1000.0,
+    };
+    account.check_balance();
+
+    account.withdraw(50.0);
+
+    account.check_balance();
+}
+
+struct BankAccount {
+    account_number: String,
+    balance: f64,
+}
+
+impl BankAccount {
+    fn withdraw(&mut self, amount: f64) {
+        println!(
+            "Withdrawing Rp. {} from account {}",
+            amount, self.account_number
+        );
+        self.balance -= amount;
+    }
+
+    fn check_balance(&self) {
+        println!(
+            "Account {} has a balance of Rp. {}",
+            self.account_number, self.balance
+        );
+    }
 }
